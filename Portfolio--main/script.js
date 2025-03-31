332f0b430e91e4f2d944f9b2f7930bd993947bad
Nanas63 <anais.mulard@gmail.com>
2025-03-31 15:13:41 +0200

    V2

--- Portfolio--main/script.js
+++ Portfolio--main/script.js
@@ -20,6 +20,8 @@ if (paragraphe) {
 })
 
 
+/* Mode light/dark */
+
 const toggleButton = document.getElementById('toggle-mode');
 const body = document.body;
 
@@ -42,7 +44,8 @@ toggleButton.addEventListener('click', () => {
 });
 
 
-/* document.addEventListener("DOMContentLoaded", function () {
+/* loader */
+document.addEventListener("DOMContentLoaded", function () {
     const loader = document.getElementById("loader");
     const content = document.getElementById("content");
 
@@ -51,13 +54,51 @@ toggleButton.addEventListener('click', () => {
         loader.style.display = "none"; // Cache le loader
         content.style.display = "block"; // Affiche le contenu principal
     }, 3000); // 3 secondes
-}); */
+});
 
 
-
+/* burger */
 const burgerButton = document.getElementById("burger-button");
 const menu = document.getElementById("menu");
 
 burgerButton.addEventListener("click", () => {
     menu.classList.toggle("active"); // Active/désactive le menu
+});
+
+
+
+/* slider controllable */
+const radios = document.querySelectorAll('.cs_anchor.radio');
+let currentIndex = Array.from(radios).findIndex(radio => radio.checked);
+
+document.getElementById('prev').addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + radios.length) % radios.length;
+  radios[currentIndex].checked = true;
+});
+
+document.getElementById('next').addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % radios.length;
+  radios[currentIndex].checked = true;
+});
+
+
+/* agrandir l'image palmiers au click */
+
+/* const image = document.getElementById("image");
+
+        image.addEventListener("click", () => {
+            // Ajoute ou enlève la classe "grande" pour agrandir/réduire l'image
+            image.classList.toggle("grande");
+        }); */
+
+
+/* Boucle pour agrandir toutes les images au clic */
+
+    const images = document.querySelectorAll(".image");
+
+    images.forEach(image => {
+    image.addEventListener("click", () => {
+        // Ajoute ou enlève la classe "grande" pour agrandir/réduire l'image
+        image.classList.toggle("grande");
+    });
 });
